dc095613263b83132d25eb5620acb3ad08cfb25f
tatsumack <tatsu.mack@gmail.com>
2019-06-09 16:31:23 +0900

    fix template

--- library/lib.hpp
+++ library/lib.hpp
@@ -8,3 +8,21 @@ int gcd(int a, int b) { return b ? gcd(b, a % b) : a; }
 
 int lcm(int a, int b) { return a * b * gcd(a, b); }
 
+template<typename T>
+vector <T> make_v(size_t a) { return vector<T>(a); }
+
+template<typename T, typename... Ts>
+auto make_v(size_t a, Ts... ts) {
+    return vector<decltype(make_v<T>(ts...))>(a, make_v<T>(ts...));
+}
+
+template<typename T, typename V>
+typename enable_if<is_class<T>::value == 0>::type
+fill_v(T& t, const V& v) { t = v; }
+
+template<typename T, typename V>
+typename enable_if<is_class<T>::value != 0>::type
+fill_v(T& t, const V& v) {
+    for (auto& e:t) fill_v(e, v);
+}
+

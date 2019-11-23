440e46867bf4a0e8ed99d32bfaac4ec03242ead5
tatsumack <tatsu.mack@gmail.com>
2019-11-23 20:58:27 +0900

    セグ木更新

--- library/seg.cpp
+++ library/seg.cpp
@@ -235,4 +235,58 @@ public:
         int vr = find(a, b, 2 * k + 2, (l + r) / 2, r);
         return max(vl, vr);
     }
-};
+}
+
+// 区間最小、区間加算
+struct LazySegmentTree {
+private:
+    int n;
+    vector<int> node, lazy;
+
+public:
+    LazySegmentTree(vector<int> v) {
+        int sz = (int) v.size();
+        n = 1;
+        while (n < sz) n *= 2;
+        node.resize(2 * n - 1);
+        lazy.resize(2 * n - 1, 0);
+
+        for (int i = 0; i < sz; i++) node[i + n - 1] = v[i];
+        for (int i = n - 2; i >= 0; i--) node[i] = min(node[i * 2 + 1], node[i * 2 + 2]);
+    }
+
+    void eval(int k, int l, int r) {
+        if (lazy[k] != 0) {
+            node[k] += lazy[k];
+            if (r - l > 1) {
+                lazy[2 * k + 1] += lazy[k];
+                lazy[2 * k + 2] += lazy[k];
+            }
+            lazy[k] = 0;
+        }
+    }
+
+    void add(int a, int b, int x, int k = 0, int l = 0, int r = -1) {
+        if (r < 0) r = n;
+        eval(k, l, r);
+        if (b <= l || r <= a) return;
+        if (a <= l && r <= b) {
+            lazy[k] += x;
+            eval(k, l, r);
+        } else {
+            add(a, b, x, 2 * k + 1, l, (l + r) / 2);
+            add(a, b, x, 2 * k + 2, (l + r) / 2, r);
+            node[k] = min(node[2 * k + 1], node[2 * k + 2]);
+        }
+    }
+
+    int find(int a, int b, int k = 0, int l = 0, int r = -1) {
+        if (r < 0) r = n;
+        eval(k, l, r);
+        if (b <= l || r <= a) return INF;
+        if (a <= l && r <= b) return node[k];
+        int vl = find(a, b, 2 * k + 1, l, (l + r) / 2);
+        int vr = find(a, b, 2 * k + 2, (l + r) / 2, r);
+        return min(vl, vr);
+    }
+};;
